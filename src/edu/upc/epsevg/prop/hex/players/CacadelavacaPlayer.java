664882d9cb4baeba2ed2e4114ca15b80af63b467
AlejandroLoboMillan <alejandrolobomillann@gmail.com>
2024-12-16 19:48:26 +0100

    minimax hecho

--- src/edu/upc/epsevg/prop/hex/players/CacadelavacaPlayer.java
+++ src/edu/upc/epsevg/prop/hex/players/CacadelavacaPlayer.java
@@ -20,78 +20,102 @@ public class CacadelavacaPlayer implements IPlayer, IAuto {
 
     @Override
     public void timeout() {
-        timeOut = true;
+        timeOut = !timeOut;
     }
 
     @Override
     public PlayerMove move(HexGameStatus s) {
-        if (timeOut) {
-            return null;  
-        }
-
-        int millorMoviment = -1;
+        int maxProfunditat = 8;
+        int profunditat = 2;
+        Point millorMoviment = null;  
         int alpha = menys_infinit;
-        for (int i = 0; i < s.getSize(); i++) {
-            for (int j = 0; j < s.getSize(); j++) {
-                if (s.getPos(i, j) == 0) { 
-                    if (millorMoviment == -1) millorMoviment = i * s.getSize() + j;  
-                    HexGameStatus copiaTauler = new HexGameStatus(s);  
-                    copiaTauler.placeStone(new Point(i, j)); 
-                    if (copiaTauler.isGameOver() && copiaTauler.GetWinner() == s.getCurrentPlayer()) {  
-                        return new PlayerMove(new Point(i, j), 0L, 0, SearchType.RANDOM);  
-                    }
-                    int valor = MinValor(copiaTauler, 3, menys_infinit, mes_infinit, s.getCurrentPlayerColor());  
-                    if (alpha < valor) {
-                        alpha = valor;
-                        millorMoviment = i * s.getSize() + j;  
+        while (!timeOut && profunditat <= maxProfunditat) {
+            for (int i = 0; i < s.getSize(); i++) {
+                for (int j = 0; j < s.getSize(); j++) {
+                    if (s.getPos(i, j) == 0) { 
+                        if (millorMoviment == null) millorMoviment = new Point(i, j);  
+
+                        HexGameStatus copiaTauler = new HexGameStatus(s);  
+                        copiaTauler.placeStone(new Point(i, j)); 
+
+                        if (copiaTauler.isGameOver() && copiaTauler.GetWinner() == s.getCurrentPlayer()) {  
+                            return new PlayerMove(new Point(i, j), 0, 0, SearchType.MINIMAX_IDS);  
+                        }
+
+                        int valor = MinValor(copiaTauler, profunditat, menys_infinit, mes_infinit, s.getCurrentPlayerColor());  
+
+                        if (alpha < valor) {
+                            alpha = valor;
+                            millorMoviment = new Point(i, j);  
+                        }
                     }
                 }
             }
+            profunditat++;
         }
-        return new PlayerMove(new Point(millorMoviment / s.getSize(), millorMoviment % s.getSize()), 0L, 0, SearchType.RANDOM);
+        timeout();
+        return new PlayerMove(millorMoviment, 0, 0, SearchType.MINIMAX_IDS);
     }
 
     private int MinValor(HexGameStatus s, int profunditat, int alfa, int beta, int color) {
+        if (timeOut) {
+            return 0;
+        }
         if (profunditat == 0 || s.isGameOver()) {  
             return 0;  
         }
+
         int valor = mes_infinit;
+
         for (int i = 0; i < s.getSize(); i++) {
             for (int j = 0; j < s.getSize(); j++) {
                 if (s.getPos(i, j) == 0) { 
                     HexGameStatus copiaTauler = new HexGameStatus(s); 
                     copiaTauler.placeStone(new Point(i, j)); 
+
                     if (copiaTauler.isGameOver() && copiaTauler.GetWinner() != s.getCurrentPlayer()) {  
                         return menys_infinit;
                     }
+
                     valor = Math.min(valor, MaxValor(copiaTauler, profunditat - 1, alfa, beta, color));
+
                     if (valor <= alfa) return valor;  
                     if (valor < beta) beta = valor;  
                 }
             }
         }
+
         return valor;
     }
 
     private int MaxValor(HexGameStatus s, int profunditat, int alfa, int beta, int color) {
+        if (timeOut) {
+            return 0;
+        }
         if (profunditat == 0 || s.isGameOver()) {  
             return 0;  
         }
+
         int valor = menys_infinit;
+
         for (int i = 0; i < s.getSize(); i++) {
             for (int j = 0; j < s.getSize(); j++) {
                 if (s.getPos(i, j) == 0) {  
                     HexGameStatus copiaTauler = new HexGameStatus(s);  
                     copiaTauler.placeStone(new Point(i, j));  
+
                     if (copiaTauler.isGameOver() && copiaTauler.GetWinner() != s.getCurrentPlayer()) {  
                         return mes_infinit;
                     }
+
                     valor = Math.max(valor, MinValor(copiaTauler, profunditat - 1, alfa, beta, color));
+
                     if (valor >= beta) return valor;  
                     if (valor > alfa) alfa = valor;  
                 }
             }
         }
+
         return valor;
     }
 
@@ -99,4 +123,5 @@ public class CacadelavacaPlayer implements IPlayer, IAuto {
     public String getName() {
         return nom;
     }
+    
 }
